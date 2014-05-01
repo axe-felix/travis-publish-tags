167deb4382857586e894c52a6a68dfbf606acebb
Felix <felix.barnsteiner@gmail.com>
2014-05-01 16:18:59 +0200

    fixed digit regex

--- etc/publish-to-github-mvn-repo-with-travis.sh
+++ etc/publish-to-github-mvn-repo-with-travis.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo `grep -F version gradle.properties` 
+echo grep -F SNAPSHOT gradle.properties: `grep -F SNAPSHOT gradle.properties` 
 echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG" 
 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 
-if [[ "$TRAVIS_BRANCH" =~ ^\d+\.\d+\.\d+ ]] && [[ "$TRAVIS_REPO_SLUG" == "axe-felix/travis-publish-tags" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+# no SNAPSHOT versions, only relase tags/branches, no forks, no pull requests
+if ! grep -Fq SNAPSHOT gradle.properties && [[ "$TRAVIS_BRANCH" =~ [0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$TRAVIS_REPO_SLUG" == "axe-felix/travis-publish-tags" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   gradle publish  
   cp -R build/repo $HOME/repo
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
+  # pssh! don't tell the secret!
   git clone --quiet https://${GH_TOKEN}@github.com/axe-felix/travis-test-mvn-repo > /dev/null
   cd travis-test-mvn-repo
   cp -R $HOME/repo .
