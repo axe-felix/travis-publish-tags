fe4e9f3c88bfb708cdb0d0736c67bc4cbfb6a524
Felix <felix.barnsteiner@gmail.com>
2014-05-01 11:00:41 +0200

    no snapshot check in shell script

--- etc/publish-to-github-mvn-repo-with-travis.sh
+++ etc/publish-to-github-mvn-repo-with-travis.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "axe-felix/travis-publish-tags" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  # publishes only if it is not a snapshot version
-  gradle publish
-  if [ -d "$build/repo" ]; then
-	  cp -R build/repo $HOME/repo
-	  cd $HOME
-	  git config --global user.email "travis@travis-ci.org"
-	  git config --global user.name "travis-ci"
-	  git clone --quiet https://${GH_TOKEN}@github.com/axe-felix/travis-test-mvn-repo > /dev/null
-	  cd travis-test-mvn-repo
-	  cp -R $HOME/repo .
-	  git add .
-	  git commit -m 'release'
-	  git push origin master > /dev/null
-  fi
+if [! grep -Fq SNAPSHOT gradle.properties] && [ "$TRAVIS_REPO_SLUG" == "axe-felix/travis-publish-tags" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  gradle publish  
+  cp -R build/repo $HOME/repo
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet https://${GH_TOKEN}@github.com/axe-felix/travis-test-mvn-repo > /dev/null
+  cd travis-test-mvn-repo
+  cp -R $HOME/repo .
+  git add .
+  git commit -m 'release'
+  git push origin master > /dev/null  
 fi
